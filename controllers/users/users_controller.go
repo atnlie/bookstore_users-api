760208abad5ce453c9b55d85276eb9e81c8e913c
Atnlie <anton@hellonimbly.com>
2021-03-05 21:35:25 +0700

    factor(user_service): add service response

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,48 @@
 package users
 
 import (
+	"atnlie/domain/users"
+	"atnlie/services"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "pake aku")
+	var user users.User
+	/*
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		//TODO: Handle error
+		fmt.Println("err -> ", err.Error())
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		//TODO: Handle json error
+		fmt.Println("err -> ", err.Error())
+		return
+	}
+
+	fmt.Println("user: ", user)
+	fmt.Println("bytes:", string(bytes))
+	fmt.Println("err: ", saveErr)
+
+	*/
+	//simplify with this
+	if err := c.ShouldBindJSON(&user); err != nil {
+		//TODO: return bad request to caller
+		fmt.Println("err -> ", err.Error())
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//TODO: handle user creation error
+		fmt.Println("err -> ", saveErr.Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,7 @@
 package services
+
+import "atnlie/domain/users"
+
+func CreateUser(user users.User) (*users.User, error){
+	return &user, nil
+}
