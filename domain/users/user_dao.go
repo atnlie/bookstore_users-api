313bdf3ef76f926fd683dc233126ce3c63683540
Atnlie <anton@hellonimbly.com>
2021-03-06 22:18:41 +0700

    feat(read-data): getUser data from table

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -13,32 +13,51 @@ var (
 )
 
 const (
-	QryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);"
-	QryGetUser    = "SELECT * FROM users WHERE id ="
-	EmailDuplicate = "Duplicate"
+	qryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);"
+	qryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id =?;"
+	emailDuplicate = "Duplicate"
 )
 
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.ClientDb.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.ClientDb.Prepare(qryGetUser)
+	if err != nil {
+		return errors.CustomInternalServerError(err.Error())
+	}
+	//this for db mode
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		fmt.Println("err -> ", err)
+		return errors.CustomNotFoundError(fmt.Sprintf("User: %d not found", user.Id))
 	}
 
-	result := userDB[user.Id]
-	if result == nil {
-		return errors.CustomNotFoundError(fmt.Sprintf("User %d not found", user.Id))
-	}
-
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
-
+	//user.Id = result.Id
+	//user.FirstName = result.FirstName
+	//user.LastName = result.LastName
+	//user.Email = result.Email
+	//user.DateCreated = result.DateCreated
 	return nil
+
+	//this for array mode
+	/*
+		result := userDB[user.Id]
+		if result == nil {
+			return errors.CustomNotFoundError(fmt.Sprintf("User %d not found", user.Id))
+		}
+
+		user.Id = result.Id
+		user.FirstName = result.FirstName
+		user.LastName = result.LastName
+		user.Email = result.Email
+		user.DateCreated = result.DateCreated
+
+		return nil
+	*/
 }
 
 func (user *User) Save() *errors.RestErr {
-	stmt, err := users_db.ClientDb.Prepare(QryInsertUser)
+	stmt, err := users_db.ClientDb.Prepare(qryInsertUser)
 	if err != nil {
 		return errors.CustomInternalServerError(err.Error())
 	}
@@ -54,7 +73,7 @@ func (user *User) Save() *errors.RestErr {
 	user.DateCreated = date_utils.GetNowString()
 	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
 	if err != nil {
-		if strings.Contains(err.Error(), EmailDuplicate) {
+		if strings.Contains(err.Error(), emailDuplicate) {
 			return errors.CustomBadRequestError(fmt.Sprintf("Email: %s already registered", user.Email))
 		}
 		return errors.CustomInternalServerError(fmt.Sprintf("Error when trying to save user: %s", err.Error()))
